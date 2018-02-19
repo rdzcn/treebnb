b21705bf5515d89db6ec7fd58d49fdad8dc20fd8
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-02-19 16:10:32 +0100

    tree controller

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,2 +1,46 @@
 class TreesController < ApplicationController
+before_action :set_tree, only: [ :show, :update, :destroy ]
+  def index
+    @trees = Tree.all
+  end
+
+  def show
+  end
+
+  def new
+    @tree = Tree.new
+  end
+
+  def create
+    @tree = Tree.new(tree_params)
+    @tree.user = current_user
+    if @tree.save
+      redirect_to tree_path(@tree)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @tree.update(tree_params)
+    redirect_to tree_path(@tree)
+  end
+
+  def destroy
+    @tree.destroy
+    redirect_to trees_path
+  end
+
+  private
+
+  def set_tree
+    @tree = Tree.find(params[:id])
+  end
+
+  def tree_params
+    params.require(:tree).permit(:location, :price_per_night, :photo, :avalable, :capacity)
+  end
 end
+
+
+
