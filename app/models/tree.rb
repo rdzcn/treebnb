af9e901f2d0881a90edebdfce9a601b69ac44eba
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-02-19 14:54:18 +0100

    tree model

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,3 +1,6 @@
 class Tree < ApplicationRecord
   belongs_to :user
+  validates :price_per_night, presence: true, numericality: true
+  validates :capacity, presence: true, numericality: true
+  validates :location, presence: true
 end
