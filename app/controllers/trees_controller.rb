20a10d47a3c8a5243342d058d7ec1995457c64ae
karenhoyos <karenuca@hotmail.com>
2018-02-20 12:53:02 +0100

    Added Pundit

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,24 +1,33 @@
 class TreesController < ApplicationController
-before_action :set_tree, only: [ :show, :update, :destroy ]
+  before_action :set_tree, only: [ :show, :update, :destroy ]
 
   def index
-    if params[:search]
+    if params[:search] == ""
+      @trees = policy_scope(Tree)
+    elsif params[:search]
+      @trees = policy_scope(Tree)
       @trees = Tree.where(location: params[:search])
     else
-      @trees = Tree.all
+      @trees = policy_scope(Tree)
+
     end
   end
 
   def show
+    @trees = Tree.find(params[:id])
+    authorize @trees
+
   end
 
   def new
     @tree = Tree.new
+    authorize @tree
   end
 
   def create
     @tree = Tree.new(tree_params)
     @tree.user = current_user
+    authorize @tree
     if @tree.save
       redirect_to tree_path(@tree)
     else
@@ -26,13 +35,21 @@ before_action :set_tree, only: [ :show, :update, :destroy ]
     end
   end
 
+  def edit
+    set_tree
+    authorize @tree
+  end
+
   def update
     @tree.update(tree_params)
+    authorize @tree
     redirect_to tree_path(@tree)
   end
 
   def destroy
+    set_tree
     @tree.destroy
+    authorize @tree
     redirect_to trees_path
   end
 
