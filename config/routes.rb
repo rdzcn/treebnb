601832ef684648834b33420d905e5d107200cb42
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2018-02-22 20:53:45 +0100

    mostly working, list disapear when you move map, need to fetch a new list - and it can not read it for some reason - alice pormised to help

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/dashboard/edit', to: 'users#edit'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'trees/update_trees', to: 'trees#update_trees', format: :js
   resources :trees do
     resources :bookings, only: [:new, :create, :show ]
   end
