b46f33768cfc1c82af6d4d50f8216edf2ab2e835
karenhoyos <karenuca@hotmail.com>
2018-02-22 15:37:41 +0100

    Feat, Added autocomplete function to the create a tree form

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -13,4 +13,41 @@ function autocomplete() {
   });
 }
 
+
+function autocompleteCreateTree() {
+  document.addEventListener("DOMContentLoaded", function() {
+    const treeLocation = document.getElementById('tree_location');
+
+    if (treeLocation) {
+      const autocomplete = new google.maps.places.Autocomplete(treeLocation, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(treeLocation, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
+
+
+
+function autocompleteAddressTree() {
+  document.addEventListener("DOMContentLoaded", function() {
+    const treeLocation = document.getElementById('tree_address');
+
+    if (treeLocation) {
+      const autocomplete = new google.maps.places.Autocomplete(treeLocation, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(treeLocation, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
+}
+
+
 export { autocomplete };
+export { autocompleteCreateTree };
+export { autocompleteAddressTree };
+
