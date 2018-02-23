e3f6e41256fc4e6d9fc1cfd854a9e83721e71d54
karenhoyos <karenuca@hotmail.com>
2018-02-23 12:05:57 +0100

    Feat, added table title to our trees model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221114716) do
+ActiveRecord::Schema.define(version: 20180223110235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180221114716) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.string "title"
     t.index ["user_id"], name: "index_trees_on_user_id"
   end
 
