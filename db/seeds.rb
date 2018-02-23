424cb4d5c4af687590f36e7449e30f3fa6c915ef
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-02-23 15:17:07 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,29 +15,4 @@ Tree.destroy_all
 User.destroy_all
 
 
-p "creating a user"
-
-user_custom = User.create(email: "arda@gmail.com", password: "123456")
-cities = ["Rudi-Dutschke-Straße, Berlin", "Fritz-Schloß-Park, Berlin", "Mauerpark, Berlin"]
-# cities = %w(Berlin Paris Havana Oslo London Shanghai Moscow Toronto York Madrit Amsterdam Prague)
-
-p "creating 20 new trees"
-
-
-
-
-
-1.upto(20) do |i|
-	Tree.create!(
-		address: cities.sample,
-		description: Faker::Commerce.department,
-    # longitude: center_point[:lng] + rand(-10.00..10.00),
-    # latitude: center_point[:lat] + rand(-10.00..10.00),
-    price_per_night: rand(20..500),
-    capacity: rand(2..73),
-    user: user_custom
-    )
-
-end
- p "finished"
 
